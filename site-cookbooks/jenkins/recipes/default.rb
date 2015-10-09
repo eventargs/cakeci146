8e92d35bb5f78467db0b2b6ad4013302808e591f
o.ohkuma <o.ohkuma@worth-t.co.jp>
2015-10-09 11:58:03 +0900

    first commit

---
+++ site-cookbooks/jenkins/recipes/default.rb
@@ -0,0 +1,15 @@
+apt_repository "jenkins" do
+  uri "http://pkg.jenkins-ci.org/debian binary/"
+  key "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+  retries 10
+  retry_delay 10
+  action :add
+end
+
+package "jenkins" do
+  action :install
+end
+
+service "jenkins" do
+  action [:enable, :start]
+end
